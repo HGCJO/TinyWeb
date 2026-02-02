d43fec7c4b569f4e4a77944e97ee2d85c3dfb103
zjt <2386501687@qq.com>
2026-02-02 15:46:03 +0800

    init lock

---
+++ lock/lock.h
@@ -0,0 +1,18 @@
+#ifndef LOCKER_H
+#define LOCKER_H
+#endif
+
+#include<exception>
+#include<pthread.h>
+#include<semaphore.h>
+
+//定义信号量
+class sem
+{
+    public:
+    sem()
+    {
+        
+    }
+
+}
